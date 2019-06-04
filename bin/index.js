58833604cf87ea2b342ebfbb0f2b4585e23adc2c
Nicolas Marteau <nico_mart@hotmail.fr>
2019-06-04 17:54:33 +0200

    chore: add envFileExist func

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,37 @@
 #!/usr/bin/env node
+
+// Require Node.js dependencies
+const { join } = require("path");
+const { readdir, readFile, writeFile } = require("fs").promises;
+const { existsSync } = require("fs");
+
+// Require Third Party dependencies
+const repos = require("repos");
+const { yellow } = require("kleur");
+
+// Globals
+require("make-promises-safe");
+require("dotenv").config({ path: join(process.cwd(), ".env") });
+
+// Constants
+
+/**
+ * @func envFileExist
+ * @desc Check if .env exist and if there is a github token.
+ * @returns {{}|{token:String}}
+ */
+function envFileExist() {
+    const envExist = existsSync(join(process.cwd(), ".env"));
+    const envToken = process.env.GITHUB_TOKEN;
+    if (envExist && envToken !== undefined) {
+        console.log("Il y a un token");
+
+        return { token: envToken };
+    }
+
+    return {};
+}
+
+function main() {
+    envFileExist()
+}
